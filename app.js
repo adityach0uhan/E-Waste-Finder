25fae5a160d3346792a02224ff4d41a090e54441
adityach0uhan <m4with6x@gmail.com>
2024-02-17 17:33:04 +0530

    Some changes

--- app.js
+++ app.js
@@ -12,16 +12,22 @@
 
 const app = require('express')();
 const PORT = 8080
-const http = require('http').Server(app);
-const cors = require('cors')
+
 // app.use(cors({ origin: 'http://127.0.0.1:5500' })) 
 
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+async function ConnectDatabase() {
+    try {
+        await mongoose.connect("mongodb+srv://admin123:admin123@cluster0.3g5r6x1.mongodb.net/?retryWrites=true&w=majority");
+        console.log("COnnected")
+    } catch(c) {
+     console.log("Database NOt connected")
+    }
+}
 
-mongoose.connect("mongodb+srv://admin123:admin123@cluster0.3g5r6x1.mongodb.net/?retryWrites=true&w=majority");
 
 //create a data schema
 const infoSchema = {
@@ -37,16 +43,12 @@ const infoSchema = {
 const user = mongoose.model("info", infoSchema);
 
 app.get("/", function (req, res) {
+    ConnectDatabase();
     res.sendFile(__dirname + "/HTML/form.html");
 })
+app.post("/",async function (req, res) {
 
-// app.get("/thankyou",function(req,res){
-//     res.sendFile(__dirname+"/index.html");
-// })
-app.post("/", function (req, res) {
-
-    async function insert() {
-        user.create({
+         await user.create({
             name: req.body.name,
             mobilenumber: req.body.mobileNumber,
             No_Of_Product: req.body.No_Of_Product,
@@ -55,9 +57,8 @@ app.post("/", function (req, res) {
             Address: req.body.address,
             pincode: req.body.pincode,
 
-        })
-    }
-    insert();
+         })
+    // res.send("User Created Successfully")
     res.sendFile(__dirname + "/HTML/submmitted.html");
 
 })
@@ -65,12 +66,5 @@ app.post("/", function (req, res) {
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
-const options = {
-    origin: 'http://localhost:8080/',
-    methods: ['GET', 'PUT ', 'POST ', 'DELETE '],
-    allowedHeaders: ['Content-Type'],
-}
-
-app.use(cors(options))
 
 
