94b6ba654526ad4bafd4074c84a5ca3b90170495
adityach0uhan <m4with6x@gmail.com>
2024-02-17 17:12:14 +0530

    Some changes

--- app.js
+++ app.js
@@ -16,62 +16,61 @@ const http = require('http').Server(app);
 const cors = require('cors')
 // app.use(cors({ origin: 'http://127.0.0.1:5500' })) 
 
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://surajkumarverma662:rp2zxvuuLKOhWSSC@cluster0.nua7vcj.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://admin123:admin123@cluster0.3g5r6x1.mongodb.net/?retryWrites=true&w=majority");
 
 //create a data schema
-const infoSchema ={
-    name:String,
-    pincode:String,
-    mobilenumber :String,
-    No_Of_Product:String,
-    dateOfPurchase:String,
-    dateOfPickup:String,
-    Address : String,
+const infoSchema = {
+    name: String,
+    pincode: String,
+    mobilenumber: String,
+    No_Of_Product: String,
+    dateOfPurchase: String,
+    dateOfPickup: String,
+    Address: String,
 }
 
-const user  = mongoose.model("info",infoSchema);
+const user = mongoose.model("info", infoSchema);
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/HTML/form.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/HTML/form.html");
 })
 
 // app.get("/thankyou",function(req,res){
-    //     res.sendFile(__dirname+"/index.html");
-    // })
-    app.post("/",function(req,res){
-        
-        
-    async function insert(){
+//     res.sendFile(__dirname+"/index.html");
+// })
+app.post("/", function (req, res) {
+
+    async function insert() {
         user.create({
-            name:req.body.name,
-            mobilenumber : req.body.mobileNumber,
-            No_Of_Product:req.body.No_Of_Product,
-            dateOfPurchase:req.body.dop,
+            name: req.body.name,
+            mobilenumber: req.body.mobileNumber,
+            No_Of_Product: req.body.No_Of_Product,
+            dateOfPurchase: req.body.dop,
             dateOfPickup: req.body.pickup,
-            Address : req.body.address,
-            pincode:req.body.pincode,
-            
+            Address: req.body.address,
+            pincode: req.body.pincode,
+
         })
-        
     }
-    res.redirect('/');
     insert();
+    res.sendFile(__dirname + "/HTML/submmitted.html");
+
 })
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
 const options = {
-    origin: 'http://127.0.0.1:5500',
+    origin: 'http://localhost:8080/',
     methods: ['GET', 'PUT ', 'POST ', 'DELETE '],
-    allowedHeaders:['Content-Type'],
-  }
+    allowedHeaders: ['Content-Type'],
+}
 
-  app.use(cors(options))
-  
+app.use(cors(options))
+
 
